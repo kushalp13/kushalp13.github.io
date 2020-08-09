3da175f9c83ce6573cce762ae75a7eaa89f14674
kushalp13 <kushal.p13@yandex.com>
2020-08-10 01:06:37 +0530

    Added Js and some more CSS

---
+++ employee_html/js/login.js
@@ -0,0 +1,27 @@
+function login()
+	{
+		var uname = document.getElementById("uname").value;
+		var pwd = document.getElementById("pwd").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("Please enter username.");
+		}
+		else if(pwd=='')
+		{
+        	alert("Enter the password");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+		}
+		else if(pwd.length < 6 || pwd.length > 6)
+		{
+			alert("Password min and max length is 6.");
+		}
+		else
+		{
+	        alert('Thank You for Login');
+            window.location = "/employee_html/home_page.html";
+		}
+	}	
---
+++ head/js/login.js
@@ -0,0 +1,18 @@
+function login()
+	{
+		var uname = document.getElementById("uname").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("Please enter username.");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+        }
+		else
+		{
+            alert('Thank You for Login');
+            window.location = "/head/home_page.html";
+		}
+	}		
